cec27d140d8d20c53c80d442756904254fb4e6f5
bullyun@aliyun.com <jiangbenli1965@gmail.com>
2020-07-09 18:04:51 +0800

    add codes

--- src/main/resources/static/js/modules/bugly/detailshow.js
+++ src/main/resources/static/js/modules/bugly/detailshow.js
@@ -1,22 +1,13 @@
 $().ready(function(){
 
      var serviceException = $("#serviceException").val();
-     $("#exception").text(serviceException);
 
-//     关键字变红色
-//     var s = "Caused by";
-//     var reg = new RegExp("(" + s + ")", "g");
-//     var newstr = str.replace(serviceException, "<font color=red>$1</font>");
-//     $("#exception").html(newstr);
-
-//      var searchText = "Caused by"；
-//      var regExp = new RegExp(searchText, "g");
-//      $("#exception").each(function()
-//             {
-//                 var html = $(this).html();
-//                 var newHtml = html.replace(regExp, "<span style='color:#ffd421' >"+searchText+"</span>");
-//                 $(this).html(newHtml);
-//             });
+     var newstr = serviceException.replace(/(Connection reset by peer)/g, "<font color=red>$1</font>")
+         .replace(/(Caused by)/g, "<font color=red>$1</font>")
+         .replace(/(com.bullyun)/g, "<font color=red>$1</font>")
+         .replace(/(manniu)/g, "<font color=red>$1</font>")
+         .replace(/(Exception)/g, "<font color=red>$1</font>");
+     $('#exception').html(newstr);
 
     var vm = new Vue({
         el: '#app',
